a65df059cf16ab8baca0750f1289657c4310f4aa
Ben Barlow <ben.barlow@yahoo.co.uk>
2022-12-09 15:25:48 +0000

    added view book function

--- controllers/controller.py
+++ controllers/controller.py
@@ -7,6 +7,11 @@ from models.books import *
 def index():
     return render_template('index.html', title='Home', books=books)
 
+@app.route('/library/<index>')
+def single_book(index):
+    selected_book = books[int(index)]
+    return render_template('book.html', book=selected_book)
+
 @app.route('/library', methods=['POST'])
 def add_book():
     title = request.form['title']
